54a15f4a6123ad5c8787903382a3f448e3bd2e85
Vincent Tourneur <vincent@pimoid.fr>
2024-04-11 16:03:06 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -4,6 +4,7 @@ cd data/
 
 if [ "$1" = jdm ]; then
   mkdir -p rezojdm16k
+  cd rezojdm16k
   wget -O "09032019-LEXICALNET-JEUXDEMOTS-FR-NOHTML.txt.zip" "https://pimo.id/98145ac7/09032019-LEXICALNET-JEUXDEMOTS-FR-NOHTML.txt.zip"
   unzip 09032019-LEXICALNET-JEUXDEMOTS-FR-NOHTML.txt.zip
   mv JDM-LEXICALNET-FR originals
@@ -22,10 +23,10 @@ elif [ "$1" = rlf ]; then
   echo -e "id\tname" | cat - rlf/originals/lffam-ids-names.csv.tmp > rlf/originals/lffam-ids-names.csv
 
   grep "cptype" rlf/originals/03-lscopolysemy-model.xml | cut -d '"' -f2,4 --output-delimiter=$'\t' > rlf/originals/cp-ids-names.csv.tmp
-  echo -e "id\tname" | cat - rlf/originals/lffam-ids-names.csv.tmp > rlf/originals/lffam-ids-names.csv
+  echo -e "id\tname" | cat - rlf/originals/cp-ids-names.csv.tmp > rlf/originals/cp-ids-names.csv
 
   grep "ls:fr:lf:" rlf/originals/14-lslf-model.xml | cut -d '"' -f2,4 --output-delimiter=$'\t' > rlf/originals/lf-ids-names.csv.tmp
-  echo -e "id\tname" | cat - rlf/originals/lffam-ids-names.csv.tmp > rlf/originals/lffam-ids-names.csv
+  echo -e "id\tname" | cat - rlf/originals/lf-ids-names.csv.tmp > rlf/originals/lf-ids-names.csv
 
 else
   echo "$0: error, must be run with \"jdm\" or \"rlf\" as argument"
